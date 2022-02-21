7c7082a04354e0dfdfcca4df04b90e3c01b7ed68
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2022-02-22 01:28:51 +0800

    clean up

--- src/mi2u/ui/CustomContainerMindow.java
+++ src/mi2u/ui/CustomContainerMindow.java
@@ -1,11 +1,9 @@
 package mi2u.ui;
 
 import arc.*;
-import arc.graphics.g2d.*;
 import arc.math.geom.Vec2;
 import arc.scene.*;
 import arc.scene.event.Touchable;
-import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
 import arc.util.*;
