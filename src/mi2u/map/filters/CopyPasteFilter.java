6da2ce449008d809efb0ba71aafd2ff24cd25673
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2022-04-16 18:57:40 +0800

    Copy Filter performs faster

--- src/mi2u/map/filters/CopyPasteFilter.java
+++ src/mi2u/map/filters/CopyPasteFilter.java
@@ -21,7 +21,7 @@ import mindustry.world.blocks.environment.StaticWall;
 import mindustry.world.blocks.environment.TreeBlock;
 
 public class CopyPasteFilter extends MI2UGenerateFilter{
-    private static Seq<TileBuffer> buffers = new Seq<>();
+    private static TileBuffer[][] buffers;
 
     private transient int worldWidth = 1, worldHeight = 1;
     public float fromX = 0, fromY = 0, width = 32, height = 32, toX = 64, toY = 64;
@@ -30,14 +30,14 @@ public class CopyPasteFilter extends MI2UGenerateFilter{
     @Override
     public FilterOption[] options(){
         return new FilterOption[]{
-                new FilterOptions.SliderOption("fromX", () -> fromX, f -> fromX = f, 0, 500f, 1),
-                new FilterOptions.SliderOption("fromY", () -> fromY, f -> fromY = f, 0, 500f, 1),
-                new FilterOptions.SliderOption("width", () -> width, f -> width = f, 1f, 500f, 1),
-                new FilterOptions.SliderOption("height", () -> height, f -> height = f, 1f, 500f, 1),
-                new FilterOptions.SliderOption("toX", () -> toX, f -> toX = f, 0f, 500f, 1),
-                new FilterOptions.SliderOption("toY", () -> toY, f -> toY = f, 0f, 500f, 1),
-                new FilterOptions.ToggleOption("block", () -> copyBlock, f -> copyBlock = f),
-                new FilterOptions.ToggleOption("overlay", () -> copyOverlay, f -> copyOverlay = f)
+            new FilterOptions.SliderOption("fromX", () -> fromX, f -> fromX = f, 0, 500f, 1),
+            new FilterOptions.SliderOption("fromY", () -> fromY, f -> fromY = f, 0, 500f, 1),
+            new FilterOptions.SliderOption("width", () -> width, f -> width = f, 1f, 500f, 1),
+            new FilterOptions.SliderOption("height", () -> height, f -> height = f, 1f, 500f, 1),
+            new FilterOptions.SliderOption("toX", () -> toX, f -> toX = f, 0f, 500f, 1),
+            new FilterOptions.SliderOption("toY", () -> toY, f -> toY = f, 0f, 500f, 1),
+            new FilterOptions.ToggleOption("targetWall", () -> copyBlock, f -> copyBlock = f),
+            new FilterOptions.ToggleOption("targetOre", () -> copyOverlay, f -> copyOverlay = f)
         };
     }
 
@@ -45,11 +45,13 @@ public class CopyPasteFilter extends MI2UGenerateFilter{
     public void apply(GenerateInput in){
         //copy to buffers before first tile is processed
         if(in.x == 0 && in.y == 0) copyToBuffers(in);
-        if(buffers.isEmpty()) return;
-        TileBuffer buffer = buffers.find(tile -> tile.x == (int)(in.x - toX + fromX) && tile.y == (int)(in.y - toY + fromY));
+        if(buffers == null) return;
+        int bufx = (int)(in.x - toX);
+        int bufy = (int)(in.y - toY);
+        if(bufx < 0 || bufx >= buffers.length || bufy < 0 || bufy >= buffers[0].length) return;
+        TileBuffer buffer = buffers[bufx][bufy];
         if(buffer == null) return;
         in.floor = buffer.floor;
-        Log.info(copyBlock + "" + copyOverlay);
         if(copyBlock) in.block = buffer.wall;
         if(copyOverlay) in.overlay = buffer.overlay;
     }
@@ -67,7 +69,7 @@ public class CopyPasteFilter extends MI2UGenerateFilter{
     }
 
     public void copyToBuffers(GenerateInput in){
-        buffers.clear();
+        buffers = new TileBuffer[(int)width][(int)height];
         worldWidth = in.width;
         worldHeight = in.height;
         GenerateInput.TileProvider provider = Reflect.get(in, "buffer");
@@ -76,26 +78,15 @@ public class CopyPasteFilter extends MI2UGenerateFilter{
             for(int y = 0; y < height; y++){
                 if(x + fromX > in.width - 1 || y + fromY > in.height - 1) continue;
                 tile = provider.get(x + (int)fromX, y + (int)fromY);
-                buffers.add(new TileBuffer(tile, x + (int)fromX, y + (int)fromY));
+                buffers[x][y] = new TileBuffer(tile);
             }
         }
     }
 
     public static class TileBuffer{
         public Block wall, floor, overlay;
-        public int x, y;
 
-        public TileBuffer(int x, int y, Block wall, Block floor, Block overlay){
-            this.x = x;
-            this.y = y;
-            this.floor = floor;
-            this.overlay = overlay;
-            this.wall = wall;
-        }
-
-        public TileBuffer(Tile tile, int x, int y){
-            this.x = x;
-            this.y = y;
+        public TileBuffer(Tile tile){
             this.floor = tile.floor();
             this.overlay = tile.overlay();
             this.wall = tile.block() instanceof Prop || tile.block() instanceof TreeBlock ? tile.block() : Blocks.air;
