9700228b1ee960ef42c7119987b75540332a046e
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2022-08-10 17:24:50 +0800

    clean up

--- src/mi2u/MI2UVars.java
+++ src/mi2u/MI2UVars.java
@@ -1,15 +1,15 @@
 package mi2u;
 
-import arc.func.Cons;
-import arc.scene.ui.TextButton;
+import arc.func.*;
+import arc.scene.ui.*;
 import arc.scene.ui.TextButton.*;
 import arc.struct.*;
-import arc.util.Align;
+import arc.util.*;
 import mi2u.ai.*;
 import mi2u.ui.*;
 import mindustry.ui.*;
 
-public class MI2UVars {
+public class MI2UVars{
     public static float titleButtonSize = 32f;
     public static TextButtonStyle textb = Styles.flatt, textbtoggle = Styles.flatTogglet;
     public static Cons<TextButton> funcSetTextb = c -> {
@@ -18,7 +18,7 @@ public class MI2UVars {
         c.margin(6f);
     };
 
-    public static Seq<Mindow2> mindow2s = new Seq<Mindow2>();
+    public static Seq<Mindow2> mindow2s = new Seq<>();
 
     public static FullAI fullAI = new FullAI();
 
