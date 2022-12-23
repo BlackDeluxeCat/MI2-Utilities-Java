cd2b9d86a90a5304c794a871d9a05d0ad1895e6c
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2022-12-23 23:41:07 +0800

    电网条重制

--- src/mi2u/ui/PowerGraphTable.java
+++ src/mi2u/ui/PowerGraphTable.java
@@ -4,22 +4,23 @@ import arc.*;
 import arc.func.*;
 import arc.graphics.*;
 import arc.math.*;
-import arc.scene.Element;
-import arc.scene.actions.Actions;
-import arc.scene.event.Touchable;
+import arc.scene.*;
+import arc.scene.event.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
 import arc.util.*;
 import mi2u.MI2UTmp;
+import mi2u.input.InputOverwrite;
 import mindustry.core.*;
 import mindustry.game.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
-import mindustry.input.DesktopInput;
+import mindustry.input.*;
 import mindustry.ui.*;
-import mindustry.world.*;
 import mindustry.world.blocks.power.*;
+
+import java.util.*;
 
 import static mindustry.Vars.*;
 
@@ -28,21 +29,23 @@ public class PowerGraphTable extends Table{
     public float barsWidth;
     public PopupTable detailTable = new PopupTable();
     private Seq<MI2Bar> bars = new Seq<>();
-    static Image[] blocksI = new Image[content.blocks().size];
+    static Element[][] blocksI = new Element[content.blocks().size][6];
 
     private Interval interval = new Interval();
     //P.G. set for ui generation, to keep P.G. order（让多电网的次序不会每次都变化）
-    private Seq<PowerGraph> saved = new Seq<>();
+    private Seq<PGInfo> saved = new Seq<>();
     private float totalCap = 0f;
 
     public PowerGraphTable(float w){
         super();
         barsWidth = w;
         update(() -> {
-            if(interval.get(0, 120)){
+            saved.each(PGInfo::update);
+            if(interval.get(0, 60)){
                 rebuild();
             }
         });
+        detailTable.touchable = Touchable.disabled;
     }
 
     public void rebuild(){
@@ -62,109 +65,139 @@ public class PowerGraphTable extends Table{
         }
 
         for(PowerGraph p : graphs){
-            if(!saved.contains(p, false)) saved.add(p);
+            if(!saved.contains(pp -> pp.pg.getID() == p.getID())) saved.add(new PGInfo(p));
             totalCap += p.getLastCapacity();
         }
 
-        saved.removeAll(s -> !graphs.contains(s));
+        saved.removeAll(s -> !graphs.contains(s.pg));
         
         int index = 0;
-        for(PowerGraph p : saved){
-            if(p.getLastCapacity() <= 0f) continue;
-            powertext = () -> barsWidth * p.getLastCapacity() / totalCap <= 50f ? "" : "" + UI.formatAmount((long)p.getLastPowerStored()) + " " + (p.getPowerBalance() >= 0 ? "+" : "") + UI.formatAmount((long)(p.getPowerBalance() * 60));
-            
-            if(index >= bars.size){
-                MI2Bar newBar = new MI2Bar();
-                newBar.userObject = p;
-                newBar.set(powertext, () -> p.getLastPowerStored() / p.getLastCapacity(), Pal.accent);
-                newBar.blink(Color.white).outline(MI2UTmp.c2.set(0.3f, 0.3f, 0.6f, 0.3f), 2f);
+        for(var info : saved){
+            if(info.pg.getLastCapacity() <= 0f) continue;
+            info.update();
+            if(index >= bars.size) bars.add(new MI2Bar());
 
-                newBar.clicked(() -> {
-                    if(control.input instanceof DesktopInput inp){
-                        Building random = ((PowerGraph)newBar.userObject).all.random();
-                        inp.panning = true;
-                        Core.camera.position.set(random.x, random.y);
-                    }
-                });
+            MI2Bar bar = bars.get(index);
+            bar.set(() -> barsWidth * info.pg.getLastCapacity() / totalCap <= 50f ? "" : "" + UI.formatAmount((long)info.pg.getLastPowerStored()) + " " + (info.pg.getPowerBalance() >= 0 ? "+" : "") + UI.formatAmount((long)(info.pg.getPowerBalance() * 60)), () -> info.pg.getLastPowerStored() / info.pg.getLastCapacity(), Pal.accent);
+            bar.blink(Color.white).outline(MI2UTmp.c2.set(0.3f, 0.3f, 0.6f, 0.3f), 2f);
 
-                newBar.hovered(() -> showDetailFor(newBar));
-                bars.add(newBar);
-            }else{
-                bars.get(index).userObject = p;
-                bars.get(index).set(powertext, () -> p.getLastPowerStored() / p.getLastCapacity(), Color.white);
-                bars.get(index).setColor(Pal.accent);
-                if(((PowerGraph)bars.get(index).userObject).getPowerBalance() < 0) bars.get(index).flash();
-            }
-            add(bars.get(index)).width(barsWidth * p.getLastCapacity() / totalCap).height(24f);
+            bar.clicked(() -> {
+                if(control.input instanceof InputOverwrite iow) iow.pan(true, MI2UTmp.v1.set(info.pg.all.random()));
+            });
+            bar.hovered(() -> showDetailFor(info, bar));
+
+            add(bars.get(index)).width(barsWidth * info.pg.getLastCapacity() / totalCap).height(24f);
             index++;
         }
     }
 
-    public void showDetailFor(MI2Bar e){
-        PowerGraph p = (PowerGraph)e.userObject;
+    public void showDetailFor(PGInfo info, MI2Bar bar){
         detailTable.shown = false;
         detailTable.background(Styles.black6);
-        detailTable.clearListeners();
         detailTable.clearChildren();
-        detailTable.touchable = Touchable.disabled;
-        detailTable.label(() -> "" + UI.formatAmount((long)(p.getLastPowerStored())) + "/" + UI.formatAmount((long)p.getLastCapacity()) + "  " + (p.getPowerBalance() >= 0 ? "+" : "") + UI.formatAmount((long)(p.getPowerBalance() * 60))).colspan(2);
-        detailTable.row();
-        detailTable.add("Prod");
-        detailTable.add("Cons");
-        detailTable.row();
-        detailTable.table(tttp -> {
-            OrderedMap<Block, Float> blocks = new OrderedMap<Block, Float>();
-            OrderedMap<Block, Float> values = new OrderedMap<Block, Float>();
+        buildInfo(detailTable, info);
 
-            for(Building producer : p.producers){
-                blocks.put(producer.block, (blocks.containsKey(producer.block) ? blocks.get(producer.block):0f) + 1f);
-                values.put(producer.block, (values.containsKey(producer.block) ? values.get(producer.block):0f) + producer.getPowerProduction() * producer.timeScale() * 60f);
-            }
-
-            int cols = (int)(blocks.size / (Core.scene.getHeight() / 36f));
-            int ir = 0;
-
-            for(Block b : blocks.keys()){
-                tttp.image(b.uiIcon).size(iconMed);
-                tttp.add("x" + blocks.get(b).intValue()).labelAlign(Align.right).padRight(8f).fontScale(1f);
-                tttp.add("+" + values.get(b).longValue()).labelAlign(Align.right).padRight(8f).fontScale(1f);
-                if(ir++ < cols) continue;
-                tttp.row();
-                ir = 0;
-            }
-        }).top();
-
-        detailTable.table(tttp -> {
-            OrderedMap<Block, Float> blocks = new OrderedMap<Block, Float>();
-            OrderedMap<Block, Float> values = new OrderedMap<Block, Float>();
-
-            for(Building consumer : p.consumers){
-                if(!consumer.block.consumesPower) continue;
-                blocks.put(consumer.block, (blocks.containsKey(consumer.block) ? blocks.get(consumer.block):0f) + 1f);
-                values.put(consumer.block, (values.containsKey(consumer.block) ? values.get(consumer.block):0f) + Mathf.num(consumer.shouldConsume()) * consumer.power.status * consumer.block.consPower.usage * 60 * consumer.timeScale());
-            }
-
-            int cols = (int)(blocks.size / (Core.scene.getHeight() / 48f));
-            int ir = 0;
-
-            for(Block b : blocks.keys()){
-                tttp.image(b.uiIcon).size(iconMed);
-                tttp.add("x" + blocks.get(b).intValue()).labelAlign(Align.right).padRight(8f).fontScale(1f);
-                tttp.add("-" + values.get(b).longValue()).labelAlign(Align.right).padRight(8f).fontScale(1f);
-                if(ir++ < cols) continue;
-                tttp.row();
-                ir = 0;
-            }
-        }).top();
         detailTable.popup();
         detailTable.update(() -> {
-            detailTable.hideWithoutFocusOn(e);
+            detailTable.hideWithoutFocusOn(bar);
             detailTable.setPositionInScreen(Core.input.mouseX() - 20f, Core.input.mouseY() - detailTable.getPrefHeight() - 20f);
         });
         detailTable.toFront();
     }
 
-    public static Image getBlockImage(Block block){
-        return blocksI[block.id] != null ? blocksI[block.id] : (blocksI[block.id] = new Image(block.uiIcon));
+    public static void buildInfo(Table table, PGInfo info){
+        table.table(t -> {
+            t.update(() -> {
+                t.clearChildren();
+                t.defaults().left().top().pad(1f);
+                t.table(tt -> {
+                    tt.defaults().left().width(100f);
+                    tt.label(() -> Iconc.blockSolarPanel + "+" + Strings.fixed(info.totalgen, 1)).colspan(2).padBottom(5f).color(Color.green);
+                    for(int m = 0; m < info.blocks.length; m++){
+                        int i = m;
+                        if(!Mathf.zero(info.bgen[i], 0.001f)){
+                            tt.row();
+                            tt.add(getBlockImage(i, 0, () -> new Image(content.block(i).uiIcon))).size(16f).pad(2f);
+                            var l = getBlockImage(i, 3, () -> new Label(""));
+                            ((Label)l).setText(() -> l.name = "+" + Strings.autoFixed(info.bgen[i], 1));
+                            tt.add(l);
+                        }
+                    }
+                });
+
+                t.table(tt -> {
+                    tt.defaults().left().width(100f);
+                    tt.label(() -> Iconc.blockSiliconSmelter + "-" + Strings.fixed(info.totalcons, 1)).colspan(2).padBottom(5f).color(Color.scarlet);
+                    for(int m = 0; m < info.blocks.length; m++){
+                        int i = m;
+                        if(!Mathf.zero(info.bcons[i], 0.001f)){
+                            tt.row();
+                            tt.add(getBlockImage(i, 1, () -> new Image(content.block(i).uiIcon))).size(16f).pad(2f);
+                            var l = getBlockImage(i, 3, () -> new Label(""));
+                            ((Label)l).setText(() -> l.name = "-" + Strings.autoFixed(info.bcons[i], 1));
+                            tt.add(l);
+                        }
+                    }
+                });
+
+                t.table(tt -> {
+                    tt.defaults().left().width(100f);
+                    tt.label(() -> Iconc.blockBattery + Strings.fixed(info.pg.getBatteryStored(), 1) + "/" + Strings.fixed(info.totalcap, 1)).colspan(2).padBottom(5f).color(Pal.accent);
+                    for(int m = 0; m < info.blocks.length; m++){
+                        int i = m;
+                        if(!Mathf.zero(info.bstore[i], 0.001f)){
+                            tt.row();
+                            tt.add(getBlockImage(i, 2, () -> new Image(content.block(i).uiIcon))).size(16f).pad(2f);
+                            var l = getBlockImage(i, 5, () -> new Label(""));
+                            ((Label)l).setText(() -> l.name = Strings.autoFixed(info.bstore[i], 1));
+                            tt.add(l);
+                        }
+                    }
+                });
+
+
+            });
+        });
+    }
+
+    public static Element getBlockImage(int id, int i3, Prov<Element> getter){
+        return blocksI[id][i3] != null ? blocksI[id][i3] : (blocksI[id][i3] = getter.get());
+    }
+
+    public static class PGInfo{
+        public PowerGraph pg;
+        public float totalcons, totalgen, totalcap;
+        public float[] bcons = new float[blocksI.length], bgen = new float[blocksI.length], bstore = new float[blocksI.length];
+        public int[] blocks = new int[blocksI.length];
+
+        public PGInfo(PowerGraph pg){
+            this.pg = pg;
+        }
+
+        public boolean vaild(){
+            return pg != null;
+        }
+
+        public void update(){
+            if(!vaild()){
+                pg = null; return;
+            }
+            Arrays.fill(blocks, 0);
+            Arrays.fill(bcons, 0);
+            Arrays.fill(bgen, 0);
+            Arrays.fill(bstore, 0);
+            totalcons = totalcap = totalgen = 0;
+
+            pg.all.each(b -> blocks[b.block.id] += 1);
+            pg.batteries.each(b -> bstore[b.block.id] += b.block.consPower.capacity);
+            pg.consumers.each(b -> bcons[b.block.id] += Mathf.zero(b.block.consPower.requestedPower(b)) ? 0f : b.block.consPower.usage * (b.shouldConsume() ? b.efficiency() * b.timeScale() : 0f) * 60f);
+            pg.producers.each(b -> bgen[b.block.id] += b.getPowerProduction() * 60f * b.efficiency() * b.timeScale());
+
+            for(int i = 0; i < blocks.length; i++){
+                totalcap += bstore[i];
+                totalcons += bcons[i];
+                totalgen += bgen[i];
+            }
+        }
     }
 }
