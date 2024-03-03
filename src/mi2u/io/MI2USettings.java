3b139db389069983ecc1e59ba59a5432d5ede82b
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2024-03-03 10:03:07 +0800

    clean up

--- src/mi2u/io/MI2USettings.java
+++ src/mi2u/io/MI2USettings.java
@@ -2,22 +2,18 @@ package mi2u.io;
 
 import arc.*;
 import arc.files.*;
-import arc.func.*;
 import arc.struct.*;
 import arc.util.*;
-import arc.util.serialization.*;
 import mindustry.*;
 
 @Deprecated
 public class MI2USettings{
     public static final OrderedMap<String, MI2USetting> map = new OrderedMap<>();
-    private static Fi root, dir;
-
-    public static Json json = new Json();
+    private static Fi dir;
 
     public static void init(){
         Core.settings.setAppName(Vars.appName);
-        root = Vars.dataDirectory.child("mods").child("MI2U_Settings");
+        Fi root = Vars.dataDirectory.child("mods").child("MI2U_Settings");
         dir = root.child("MI2USettings.mi2u");
         load();
     }
@@ -29,42 +25,6 @@ public class MI2USettings{
         }else{
             new MI2USetting(name, value);
         }
-    }
-
-    public static int getInt(String name, int def){
-        MI2USetting obj = map.get(name);
-        if(obj == null) return def;
-        return Strings.parseInt(obj.value, def);
-    }
-
-    public static int getInt(String name){
-        return getInt(name, 0);
-    }
-
-    public static String getStr(String name, String def){
-        MI2USetting obj = map.get(name);
-        if(obj == null) return def;
-        return obj.value;
-    }
-
-    public static String getStr(String name){
-        return getStr(name, "");
-    }
-
-    public static boolean getBool(String name, boolean def){
-        MI2USetting obj = map.get(name);
-        if(obj == null) return def;
-        return obj.value.equals("true");
-    }
-
-    public static boolean getBool(String name){
-        return getBool(name, false);
-    }
-
-    public static <T> T getJson(String name, Class<T> clazz, Class elementClazz, Prov<T> def){
-        MI2USetting obj = map.get(name);
-        if(obj == null) return def.get();
-        return json.fromJson(clazz, elementClazz, obj.value);
     }
 
     public static boolean load(){
@@ -91,7 +51,7 @@ public class MI2USettings{
             Log.infoTag("MI2U", index + " Settings loaded.");
             reads.close();
         }catch(Throwable e){
-            Log.errTag("MI2U", "Settings load failed: " + e.toString());
+            Log.errTag("MI2U", "Settings load failed: " + e);
             return false;
         }
 
