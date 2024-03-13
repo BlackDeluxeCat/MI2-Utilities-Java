e7743f9ccaa3fbb6055a3aa975d36434c2ef0e1b
BlackDeluxeCat <65377021+BlackDeluxeCat@users.noreply.github.com>
2024-03-13 22:25:09 +0800

    desktop print F selection size

--- src/mi2u/input/DesktopInputExt.java
+++ src/mi2u/input/DesktopInputExt.java
@@ -1,14 +1,19 @@
 package mi2u.input;
 
 import arc.*;
+import arc.graphics.*;
+import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.math.geom.*;
 import arc.scene.ui.layout.*;
 import arc.util.*;
 import mi2u.*;
 import mi2u.graphics.*;
+import mindustry.content.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.input.*;
+import mindustry.ui.*;
 import mindustry.world.blocks.*;
 
 import static arc.Core.*;
@@ -174,5 +179,18 @@ public class DesktopInputExt extends DesktopInput implements InputOverwrite{
             fullT.margin(0f);
             fullT.layout();
         }
+    }
+
+    @Override
+    protected void drawSelection(int x1, int y1, int x2, int y2, int maxLength){
+        super.drawSelection(x1, y1, x2, y2, maxLength);
+        Placement.NormalizeDrawResult result = Placement.normalizeDrawArea(Blocks.air, x1, y1, x2, y2, false, maxLength, 1f);
+        Font font = Fonts.outline;
+        float oldScaleX = font.getScaleX();
+        float oldScaleY = font.getScaleY();
+        font.getData().setScale(4f / Scl.scl(1f) * camera.width / graphics.getWidth());
+        font.getCache().setColor(MI2UTmp.c1.set(Pal.accent).a(0.7f));
+        font.draw((int)(result.x2 - result.x) / 8 + "x" + (int)(result.y2 - result.y) / 8, result.x2, result.y, Align.right);
+        font.getData().setScale(oldScaleX, oldScaleY);
     }
 }
